4933664affbcf60fae404f6c4eee5fd8105277d2
142spp <ldg142s@gmail.com>
2026-03-30 22:11:11 +0900

    260330

---
+++ codes/260330/11725.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    int n; cin >> n;
+    vector<int> v(n+1);
+    vector<vector<int>> adj(n+1);
+    for(int i=0; i<n; i++){
+        int a, b; cin >> a >> b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+    queue<int> q; q.push(1);
+    bool ans[100001] = {};
+    while(!q.empty()){
+        auto f = q.front(); q.pop();
+        for(auto a : adj[f]){
+            if(ans[a]) continue;
+            ans[a] = f;
+            q.push(a);
+        }
+    }
+    for(int i=1; i<n; i++){
+        cout << ans[i] << "\n";
+    }
+}
